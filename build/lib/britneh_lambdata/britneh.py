fc79585ec35a8e9ac776b6a316dc5e7cf5992f25
britneh <brhacket@gmail.com>
2020-06-02 14:16:48 -0700

    updating and adding files, moved setup to root

--- build/lib/britneh_lambdata/britneh.py
+++ build/lib/britneh_lambdata/britneh.py
@@ -8,16 +8,17 @@ def add_state_names_column(my_df):
     Params (my_df) a Dataframe with a column called " abbrev" that has state abbrev
     return a copy of the original dataframe, but with an extra column
     """
-    new_df = my_df.copy()
+    new_df = my_df_copy()
     names_map = {
         "CT": "Connecticut",
         "CO": "Colorado",
         "CA": "Cali",
         "TX": "Texas"}
     new_df["name"] = new_df["abbrev"].map(names_map)
-    return new_df
+
 
 if __name__ == "__main__":
+    .
     print(df.head)
     mapped_df = add_state_names_column(df)
     print(mapped_df.head())
--- setup.py
+++ setup.py
@@ -4,15 +4,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="britneh_lambadata", # the name that you will install via pip
+    name="britneh_lambadata",  # the name that you will install via pip
     version="1.0",
     author="Britne Hackett",
     author_email="brhacket@email.com",
     description="A couple of helpful functions",
     long_description=long_description,
-    long_description_content_type="text/markdown", # required if using a md file for long desc
-    #license="MIT",
+    long_description_content_type="text/markdown",
+    # required if using a md file for long desc
+    # license="MIT",
     url="https://github.com/britneh/lambdata---unit-3",
-    #keywords="",
-    packages=find_packages() # ["my_lambdata"]
+    # keywords="",
+    packages=find_packages()  # ["my_lambdata"]
 )
